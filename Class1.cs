a427d419a71e142a8f1401850871c39927a1d97d
LimS123 <romanii200219@gmail.com>
2020-11-01 22:02:51 +0300

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabRab6
+{
+    public abstract partial class IntelligentCreature
+    {
+        public string date { get; set; }
+        public virtual void Display()
+        {
+        }
+    }
+}
